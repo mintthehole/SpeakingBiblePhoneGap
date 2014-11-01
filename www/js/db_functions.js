e43693882e0af3c5ce58ef42d32caf6b13d667c2
mintthehole <john.pollo@strata.co.in>
2014-11-01 15:29:58 +0530

    Included css and js

--- www/js/db_functions.js
+++ www/js/db_functions.js
@@ -2,6 +2,8 @@
 function populateDB(tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS BOOKS (id unique, s_title,title)');
     tx.executeSql('CREATE TABLE IF NOT EXISTS CHAPTERS (id unique,book_s_title,chapter_no,url)');
+    tx.executeSql('DROP TABLE VERSES');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS VERSES (order_no,book_s_title,verse_no,verse_start,chapter_text,verse_text,chapter_no,book,paragraph)');
 }
 
 function createBooks(tx) {
@@ -9,20 +11,30 @@ function createBooks(tx) {
 }
 
 function createChapters(tx) {
-    alert("pollo");
     tx.executeSql('SELECT * FROM CHAPTERS', [], querySuccessChapters, errorCB);
 }
 
+function createVerses(tx){
+    tx.executeSql('SELECT * FROM VERSES', [], querySuccessVerses, errorCB);
+}
 
+function querySuccessVerses(tx, results) {
+    var len = results.rows.length;
+    if (len == 0)
+    {
+        verseCreation(tx);
+        alert("Done");
+    }
 
+}
 
 function querySuccessChapters(tx, results) {
     var len = results.rows.length;
     if (len == 0)
     {
-
         create_chapters(tx);
     }
+    createVerses(tx);
 }
 
 function querySuccessBooks(tx, results) {
